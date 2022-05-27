17400e06a84c8523a8084ae2e90720fd7d095eeb
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-05-27 08:17:14 -0500

    pseudocoding done

---
+++ Develop/work.js
@@ -0,0 +1,11 @@
+//current date
+//get time and daate format from moment.js
+//declare variables
+//variable to
+//need to save in local storage
+//set event handler for all save buttons
+//need button function to clear local storage and clear contents
+//function to take each hour and compare to the actual time
+//get items from local storage and set it in proper places
+//sets date in header and current time
+//
