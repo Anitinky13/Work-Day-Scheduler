8e3fc5e1290f8b1e3bafd79ea435563ddb6522c5
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-05-27 10:06:47 -0500

    local storage

--- Develop/work.js
+++ Develop/work.js
@@ -1,9 +1,29 @@
+$(document).ready(function () {
+  //get time and daate format from moment.js
+  var NowMoment = moment().format("MMMM Do YYYY");
+  var displayDate = document.getElementById;
+  ("todayDay");
+  displayDate.innerHTML = NowMoment;
+});
+//when user clicks on save btn
+$(document).ready(function () {
+  $(".saveBtn").on("click", function (event) {
+    event.preventDefault();
+    var text = $(this).siblings(".userInput").val();
+    var time = $(this).parent().attr("id");
+
+    //need to save in local storage
+
+    localStorage.setItem(time, text);
+  });
+});
+
 //current date
-//get time and daate format from moment.js
-var myDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+
 //declare variables
 //variable to
 //need to save in local storage
+
 //set event handler for all save buttons
 //need button function to clear local storage and clear contents
 //function to take each hour and compare to the actual time
