05b686b1aa6a31a19c0e8f1ceb9b63a947e8ac97
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-05-27 13:52:38 -0500

    css styles

--- Develop/work.js
+++ Develop/work.js
@@ -1,32 +1,53 @@
 $(document).ready(function () {
   //get time and daate format from moment.js
-  var NowMoment = moment().format("MMMM Do YYYY");
-  var displayDate = document.getElementById;
-  ("todayDay");
-  displayDate.innerHTML = NowMoment;
-});
-//when user clicks on save btn
-$(document).ready(function () {
-  $(".saveBtn").on("click", function (event) {
-    event.preventDefault();
-    var text = $(this).siblings(".userInput").val();
-    var time = $(this).parent().attr("id");
 
-    //need to save in local storage
+  //current date
+  var currentDay = moment().format("MMMM Do YYYY, HH:mm");
+  $("#currentDay").text(currentDay);
 
-    localStorage.setItem(time, text);
+  var toDoAlert = function () {
+    var curTime = moment().format("H");
+    $("textarea").each(function () {
+      var hour = parseInt($(this).attr("text"));
+
+      if (hour == curTime) {
+        $(this).addClass("Present");
+      } else if (hour > curTime) {
+        $(this).addClass("Future");
+      } else {
+        $(this).addClass("Past");
+      }
+    });
+  };
+  toDoAlert();
+
+  //when user clicks on save btn
+  $(document).ready(function () {
+    $(".saveBtn").on("click", function (event) {
+      event.preventDefault();
+      var text = $(this).siblings(".Description").val();
+      var time = $(this).parent().attr("id");
+      //need to save in local storage
+      localStorage.setItem(time, text);
+      //localStorage.getItem(".Description");
+    });
+
+    //set event handler for all save buttons
+
+    //need button function to clear local storage and clear contents
+
+    //function to take each hour and compare to the actual time
+
+    //get items from local storage and set it in proper places
+    $("#hour-9.Description").val(localStorage.getItem("9"));
+    $("hour-10.Description").val(localStorage.getItem("10"));
+    $("hour-11.Description").val(localStorage.getItem("11"));
+    $("hour-12.Description").val(localStorage.getItem("12"));
+    $("hour-1.Description").val(localStorage.getItem("1"));
+    $("hour-2.Description").val(localStorage.getItem("2"));
+    $("hour-3.Description").val(localStorage.getItem("3"));
+    $("hour-4.Description").val(localStorage.getItem("4"));
+    $("hour-5.Description").val(localStorage.getItem("5"));
   });
+  //sets date in header and current time
 });
-
-//current date
-
-//declare variables
-//variable to
-//need to save in local storage
-
-//set event handler for all save buttons
-//need button function to clear local storage and clear contents
-//function to take each hour and compare to the actual time
-//get items from local storage and set it in proper places
-//sets date in header and current time
-//
